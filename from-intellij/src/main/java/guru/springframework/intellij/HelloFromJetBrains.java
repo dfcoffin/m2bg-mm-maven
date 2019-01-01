82b10254c5a9eccd85b318db79e6aa00d555b6f0
Donald F. Coffin <(donald.coffin@reminetworks.com)>
2019-01-01 01:41:40 -0500

    1) Adding moduel from-intellij

---
+++ from-intellij/src/main/java/guru/springframework/intellij/HelloFromJetBrains.java
@@ -0,0 +1,12 @@
+package guru.springframework.intellij;
+
+/**
+ * Created by Donald F. Coffin on 2019-01-01 at 01:35
+ **/
+public class HelloFromJetBrains {
+
+    public String sayHello() {
+
+        return "Hello from JetBrains!";
+    }
+}
---
+++ from-intellij/src/test/java/package-info.java
@@ -0,0 +1,3 @@
+/**
+ * Created by Donald F. Coffin on 2019-01-01 at 01:38
+ **/
