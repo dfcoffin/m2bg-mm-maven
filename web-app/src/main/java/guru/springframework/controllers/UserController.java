877d2a423c22d6d0da3455bf062c56e164a859a0
Donald F. Coffin <(donald.coffin@reminetworks.com)>
2019-01-01 13:14:02 -0500

    1) Adding web-app example

---
+++ web-app/src/main/java/guru/springframework/controllers/UserController.java
@@ -0,0 +1,16 @@
+package guru.springframework.controllers;
+
+import guru.springframework.domain.UserCommand;
+import guru.springframework.entities.User;
+
+/**
+ * Created by Donald F. Coffin on 2019-01-01 at 13:00
+ **/
+public class UserController {
+
+    User saveUser(UserCommand command) {
+
+        // Fake Implementation
+        return new User();
+    }
+}
